97840dbb3eb719f5f9fb33a2140578671a558767
stormlex <alex@stormlike.com>
2019-06-03 10:33:24 +0200

    boardwalk

--- keyboards/boardwalk/keymaps/stormlex/keymap.c
+++ keyboards/boardwalk/keymaps/stormlex/keymap.c
@@ -28,33 +28,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  /* 2x2u Space with Arrows
  * .-----------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | -      | =      | BACKSP |
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | -      | =      | DEL    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      | [      | ]      | DEL    |
+ * | TAB    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      | [      | ]      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | CAP LK | A      | S      | D      | F      | G      | H      | J      | K      | L      | ;      | "      | ENTER  |    *   |
+ * | BACKSP | A      | S      | D      | F      | G      | H      | J      | K      | L      | ;      | "      | ENTER  |    *   |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | N      | M      | ,      | .      | /      | SHIFT  | UP     | ISO|   |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LGUI   |  PgUp  | PgDn   |  LALT  |      SPACE      | RALT   |  Fn    | PrtScr | RCTRL  | LEFT   | DOWN   | RIGHT  |
+ * | LCTRL  | LGUI   |  LALT  |                        SPACE                        |  Ralt  |   Fn   | LEFT   | DOWN   | RIGHT  |
  * '-----------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_BASE] = LAYOUT_1u2_arrow(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, 
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_DEL,  
-  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_BSLS, 
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL, 
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,  
+  KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_BSLS, 
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_NUBS, 
-  KC_LCTL, KC_LGUI, KC_PGUP, KC_PGDN, KC_LALT,     KC_SPC,       KC_RALT, FN,      KC_PSCR, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  
+  KC_LCTL, KC_LGUI, KC_LALT, KC_NO,   KC_NO,   KC_SPC,       KC_NO,       KC_NO,   KC_RALT, FN,      KC_LEFT, KC_DOWN, KC_RGHT  
  ),
 
 
   [_FN] = LAYOUT_1u2_arrow(
-  KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, 
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-  RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-  _______, RGB_TOG, RGB_MOD, RGB_SAI, RGB_HUI, RGB_SAD, RGB_HUD, RGB_VAD, RGB_VAI, _______, _______, _______, _______, _______, 
-  _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______, _______, _______
+  KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, 
+  RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, 
+  KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, 
+  _______, RGB_TOG, RGB_MOD, RGB_SAI, RGB_HUI, RGB_SAD, RGB_HUD, RGB_VAD, RGB_VAI, _______, _______, _______, KC_PGUP, KC_VOLD, 
+  _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
  )
 
 
