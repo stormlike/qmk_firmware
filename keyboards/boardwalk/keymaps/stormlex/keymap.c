41d84cb9b015ac81d2ddd82f977c9017a68bb80d
stormlex <alex@stormlike.com>
2019-06-11 15:23:18 +0200

    update

--- keyboards/boardwalk/keymaps/stormlex/keymap.c
+++ keyboards/boardwalk/keymaps/stormlex/keymap.c
@@ -45,15 +45,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,  
   KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_BSLS, 
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_NUBS, 
-  KC_LCTL, KC_LGUI, KC_LALT, KC_NO,   KC_NO,   KC_SPC,       KC_NO,       KC_NO,   KC_RALT, FN,      KC_LEFT, KC_DOWN, KC_RGHT  
+  KC_LCTL, KC_MUTE, KC_VOLD, KC_VOLU, KC_LALT,   KC_SPC,       KC_RALT,   FN,      KC_LGUI, KC_RCTRL, KC_LEFT, KC_DOWN, KC_RGHT  
  ),
 
 
   [_FN] = LAYOUT_1u2_arrow(
   KC_GRAVE,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, 
-  RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, 
-  KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, 
-  _______, RGB_TOG, RGB_MOD, RGB_SAI, RGB_HUI, RGB_SAD, RGB_HUD, RGB_VAD, RGB_VAI, _______, _______, _______, KC_PGUP, KC_VOLD, 
+  RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+  KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+  _______, RGB_TOG, RGB_MOD, RGB_SAI, RGB_HUI, RGB_SAD, RGB_HUD, RGB_VAD, RGB_VAI, _______, _______, _______, KC_PGUP, _______, 
   _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
  )
 
